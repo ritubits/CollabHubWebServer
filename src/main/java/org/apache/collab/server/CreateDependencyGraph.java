5ca008d90446c7be3a1e6cfa6e1289321ad07138
rituarora2013 <mailtorituarora@yahoo.com>
2016-04-06 20:21:06 +0530

    Created class and method nodes

--- src/main/java/org/apache/collab/server/CreateDependencyGraph.java
+++ src/main/java/org/apache/collab/server/CreateDependencyGraph.java
@@ -5,8 +5,17 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.HashSet;
 import java.util.Set;
 
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.SimpleName;
+import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 import org.neo4j.graphdb.factory.GraphDatabaseBuilder;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.neo4j.graphdb.GraphDatabaseService;
@@ -35,6 +44,8 @@ import org.eclipse.jdt.core.dom.MethodDeclaration;
 
 
 
+
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,6 +53,9 @@ public class CreateDependencyGraph {
 
 	
 	 private final String DB_PATH = "target-DB-Client2/neo4j-dependencyGraph-db";
+	 private static final String SRC_URL = "D:\\TestGitProjectRepo\\ParallelCollab\\Ass1\\src";
+	 private String projectName;
+	 private dependencyGraphNodes dpGraph;
 	 
 	      GraphDatabaseService graphDb;
 	      Node rootNode;
@@ -51,25 +65,27 @@ public class CreateDependencyGraph {
 	    	PROJECT, PACKAGE, CLASS, METHOD, ATTRIBUTE;
 	    }   
 	    
-	    Relationship relationship;
+
 	    
-	    public static enum RelTypes implements RelationshipType
-	    {
-	    	CONNECTING, DEPENDENCY;
-	    }
-	    
-/*	    public static void main(String args[]) {
+	    public static void main(String args[]) {
 
 	       	CreateDependencyGraph db= new CreateDependencyGraph();
-	       	db.initializeDB();
+	       	db.initializeDB(SRC_URL, "CollabProject");
 				
 		}
-	*/    
+   
 	    
-	    public void initializeDB() {
+	    public void initializeDB(String srcURL, String pName) {
 			
 	    	try {
-	       	 
+	    		projectName = pName;
+	    		dpGraph = new dependencyGraphNodes();
+	    		System.out.println(srcURL);
+	    		File root = new File(srcURL);
+	    		//System.out.println(root.listFiles());
+	    		File[] files = root.listFiles ( );
+	    	//	parseFiles(files);
+	    		
 	    		clearDb();
 	    		File dbDir = new File(DB_PATH);
 	    		GraphDatabaseFactory graphFactory = new GraphDatabaseFactory();
@@ -77,7 +93,7 @@ public class CreateDependencyGraph {
 	    		 graphDb = graphBuilder.newGraphDatabase();                  
 	            registerShutdownHook( graphDb );
 
-				createDB();
+				createDB(files);
 				shutDown(graphDb);	
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
@@ -86,15 +102,8 @@ public class CreateDependencyGraph {
 	    	System.out.println("Out of DB");
 				
 		}
-	    
-	public   void createDB() throws Exception
-	{
-		
-		rootNode= createGraphAST();	
 
-	}//end of createDB
-
-	public Node createGraphAST() throws Exception
+	public Node createDB(File[] files) throws Exception
     {
 
 
@@ -115,11 +124,33 @@ public class CreateDependencyGraph {
       
         	System.out.println("created rootNode object");
 
-        	rootNode.setProperty( "name", "CollabProject" );
-        	rootNode.setProperty("type", dGraphNodeType.PROJECT);
+        	rootNode.setProperty( "name", projectName );
+        	rootNode.setProperty("nodeType", "PROJECT");
+        	rootNode.addLabel(dGraphNodeType.PROJECT);
 
-        	
-        	System.out.println("setting root node properties");
+    		//call createGraphAST for each file
+         	String filePath = null;
+         	Node cNode= null;
+         	for (File f : files ) {
+     			 filePath = f.getAbsolutePath();
+     			System.out.println(filePath);
+     			 if(f.isFile() && (f.getName().contains(".java"))){
+     				 //print filename
+     				 System.out.println("In file: "+ f.getName());
+     				 
+     				 //add class node with fileName f.getName()
+     				 cNode= dpGraph.addConnectingClassNode(graphDb, rootNode, f.getName(), "default", "default","default", "default");
+     				
+     				 CompilationUnit cu = parse(readFileToString(filePath));
+     				 //for each file, get its Methods and add nodes
+     				 getMethodGraph(cu, dpGraph, graphDb, cNode);
+     				 
+     				 //pass the entire AST of a file to create the dependency graph
+     				 //dpGraph.createDependencyGraph(cu, graphDb, rootNode,f.getName());
+     			 }
+     		//	 System.out.println("here123");
+     		 }
+        	System.out.println("created graph");
 
             // START SNIPPET: transaction
             tx.success();
@@ -133,7 +164,7 @@ public class CreateDependencyGraph {
         finally {
         	
         	 if (tx != null) {
-        		 tx.finish();
+        		 tx.close();
         		 System.out.println("Closing transaction object");
         		 
              }
@@ -143,6 +174,53 @@ public class CreateDependencyGraph {
         return rootNode;
     }
 
+    
+		public   void createGraphAST(File[] files) throws Exception
+		{
+			
+		//	rootNode= createGraphAST();	
+			
+		
+		
+		}//end of createGraphAST
+
+		//read file content into a string
+		public static String readFileToString(String filePath) throws IOException 
+		{
+			StringBuilder fileData = new StringBuilder(1000);
+			BufferedReader reader = new BufferedReader(new FileReader(filePath));
+	 
+			char[] buf = new char[10];
+			int numRead = 0;
+			while ((numRead = reader.read(buf)) != -1) {
+	
+				String readData = String.valueOf(buf, 0, numRead);
+				fileData.append(readData);
+				buf = new char[1024];
+			}
+			reader.close();
+			return  fileData.toString();	
+		}
+		
+		//use ASTParse to parse string
+		public static CompilationUnit parse(String str) {
+			// each str contains the str content of a single java file
+			ASTParser parser = ASTParser.newParser(AST.JLS3);
+			parser.setSource(str.toCharArray());
+			parser.setKind(ASTParser.K_COMPILATION_UNIT);
+	 
+			 // In order to parse 1.5 code, some compiler options need to be set to 1.5
+			 Map options = JavaCore.getOptions();
+			 JavaCore.setComplianceOptions(JavaCore.VERSION_1_5, options);
+			 parser.setCompilerOptions(options);
+			 
+			final CompilationUnit cu = (CompilationUnit) parser.createAST(null);
+
+			return cu;
+			
+			
+		}
+	 
 	 private  void registerShutdownHook( final GraphDatabaseService graphDb )
 	    {
 	        // Registers a shutdown hook for the Neo4j instance so that it
@@ -158,7 +236,7 @@ public class CreateDependencyGraph {
 	        } );
 	    }
 	
- /*   public  void getMethodGraph(final CompilationUnit cu, final DependencyGraphEx dpGraph, final GraphDatabaseService graphDb, final Node cNode) {
+    public  void getMethodGraph(final CompilationUnit cu, final dependencyGraphNodes dpGraph, final GraphDatabaseService graphDb, final Node cNode) {
 		
   		cu.accept(new ASTVisitor() {
    
@@ -182,7 +260,13 @@ public class CreateDependencyGraph {
   			});
    
   	} 		
-    */
+    
+	 
+	 public void parseFiles(File[] files)
+	 {
+     	//parsing files
+
+	 }
     void shutDown(GraphDatabaseService graphDb)
     {
         System.out.println("line 227");
